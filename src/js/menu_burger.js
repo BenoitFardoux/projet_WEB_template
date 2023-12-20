498e9b8cc5d46c1e9084cf25cdf37815f66596ac
Benoit <fardouxbenoit@gmail.com>
2023-12-20 09:47:12 +0100

    feature(burger) :  ajout burger dynamique

--- src/js/menu_burger.js
+++ src/js/menu_burger.js
@@ -1,26 +1,51 @@
 let isBurgerOpen = false;
 let innerBurger = document.getElementById("");
 let burgerMenu = document.getElementById("");
+
+
+let burgerContent = [
+    {
+        nomCategorie: "a",
+        lien : "lien categorie1",
+        contenu: [
+            {
+                nomCategorie: "cat a", lien: "lien a"
+            },
+            {
+                nomCategorie: "cat b", lien: "lien b"
+            }]
+    },
+    {
+        nomCategorie: "b",
+        contenu: [
+            {
+                nomCategorie: "cat a", lien: "lien a"
+            },
+            {
+                nomCategorie: "cat b", lien: "lien b"
+            }]
+    }
+]
+
 window.onload = () => {
     burgerMenu = document.getElementById("menu_burger");
     burgerMenu.addEventListener("click", toggleBurgerClicked);
     innerBurger = document.getElementById("innerBurger");
+    addItemInBurgerMenu(innerBurger)
 }
-
-
 
 
 const open = () => {
     innerBurger.classList.remove("animateSlideOpen")
     burgerMenu.addEventListener("click", toggleBurgerClicked)
-    innerBurger.removeEventListener("animationend",open)
+    innerBurger.removeEventListener("animationend", open)
 }
 
 const close = () => {
     innerBurger.classList.remove("animateSlideClose")
     innerBurger.classList.add("hidden")
     burgerMenu.addEventListener("click", toggleBurgerClicked);
-    innerBurger.removeEventListener("animationend",close)
+    innerBurger.removeEventListener("animationend", close)
 }
 /**
  * display or hide the content of burger menuZz
@@ -39,4 +64,20 @@ const toggleBurgerClicked = () => {
         innerBurger.classList.add("animateSlideClose")
         innerBurger.addEventListener("animationend", close)
     }
+}
+
+const addItemInBurgerMenu = (innerBurger) => {
+    burgerContent.forEach((categorie) => {
+        const categorieElement = document.createElement("div")
+
+        categorieElement.classList.add("burger-category")
+        categorieElement.innerText = `${categorie.nomCategorie}`
+        innerBurger.appendChild(categorieElement)
+        categorie.contenu.forEach((sousCategorie) => {
+            const subCategoryElement = document.createElement("a")
+            subCategoryElement.innerText = sousCategorie.nomCategorie
+            subCategoryElement.href = sousCategorie.lien
+            innerBurger.appendChild(subCategoryElement)
+        })
+    })
 }
